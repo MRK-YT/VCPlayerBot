9cfde4881de256d259d9a498d75bcbe4a0b21bbc
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-10-04 20:37:41 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -402,8 +402,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data=="help":
             buttons = [
                 [
-                    InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-                    InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/VCPlayerBot'),
+                    InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/Mo_Tech_YT'),
+                    InlineKeyboardButton('🧩 Source', url='https://github.com/MRK-YT/VCPlayerBot'),
                 ]
             ]
             reply_markup = InlineKeyboardMarkup(buttons)
